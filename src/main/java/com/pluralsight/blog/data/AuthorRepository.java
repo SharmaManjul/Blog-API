f03cf14d4c68a3ac20959504278e2bcc6c709eb3
SharmaManjul <manjul199912@gmail.com>
2022-03-26 11:25:31 -0500

    Removed Authors as an endpoint and authos name is accessible through the posts.

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,9 +2,11 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long>{}
