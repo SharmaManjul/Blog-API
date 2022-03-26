22423717e141bb4c5551ca3797cee076f575bc89
SharmaManjul <manjul199912@gmail.com>
2022-03-26 11:18:26 -0500

    Added search blogs by author last names.

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long>{
     @RestResource(rel="contains-title", path="containsTitle")
     public List<Post> findByTitleContaining(String Title);
+    public List<Post> findByAuthor_Lastname(String lastname);
 }
